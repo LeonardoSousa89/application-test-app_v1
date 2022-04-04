67972667785f7e7dd76fc39b40d3c33cf37f448f
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-04-04 00:12:04 -0300

    master

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const _port     = 3003
 
 const server    = require('./server/index')
+const log       = require('morgan') 
 const express   = require('express')
 const app       = express()
 
+app.use(log('dev'))
 app.use('/', server)
+app.use('/static', express.static('public'));
 
 app.listen(process.env.PORT || _port,()=>{
     console.log(`localhost:${_port}`)
--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express   = require('express')
 const server    = express.Router()
 
-server.route('/').get((req,res)=>{
-    res.sendFile(__dirname + '/login/index.html')
+
+
+server.route('/app/login').get((req,res)=>{
+    res.sendFile(__dirname + '/views/login/index.html')
 })
+
+server.route('/app/create-account').get((req,res)=>{
+    res.sendFile(__dirname + '/views/account/index.html')
+})
+
+
 
 module.exports = server
